2abfc66ea494e7cad1d6a25859157b3e53517063
duck <al.wenn05@gmail.com>
2025-10-22 14:26:23 -0700

    Added sus and mana bars, casting spells

--- Player/player.gd
+++ Player/player.gd
@@ -1,12 +1,32 @@
 extends Area2D
 
+const MAX_MANA = 10
+var mana = 5
+
+const MAX_SUS = 5
+var sus = 1
+
 # player stats
 @export var speed = 400
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	update_mana_ui()
+	update_sus_ui()
+	$mana.max_value = MAX_MANA
+	$sus.max_value = MAX_SUS
+	
+func update_mana_ui():
+	set_mana_bar()
 
+func set_mana_bar() -> void:
+	$mana.value = mana
+	
+func update_sus_ui():
+	set_sus_bar()
+
+func set_sus_bar() -> void:
+	$sus.value = sus
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -26,4 +46,16 @@ func _process(delta: float) -> void:
 	# update player position based on velocity
 	position += velocity * delta
 	# position = position.clamp(Vector2.ZERO)
+	
+	if (Input.is_action_just_pressed("cast_spell") && mana >= 1):
+		mana -= 1
+	
+	if (mana < MAX_MANA):
+		mana += delta
+	else:
+		position = Vector2.ZERO
+		mana = 0
+	
+	update_mana_ui()
+	update_sus_ui()
 	
