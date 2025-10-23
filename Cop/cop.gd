a86c3a34f90b4a8b437045bb2f6ac85fc7016a74
Edison Chan <edisonbenjaminc@gmail.com>
2025-10-22 22:54:16 -0700

    cop suspicion
    cops get suspicious if player casts magic, noted by a lil question mark above their head

--- Cop/cop.gd
+++ Cop/cop.gd
@@ -1,23 +1,28 @@
-extends Area2D
+extends RigidBody2D
 
 var target_pos = Vector2.ZERO
-var target_entity
 var speed = 50
 
-const search_dist = 20
+const search_dist_max = 25
+const search_dist_min = 10
 
 const wander_time = 10
-var curr_time = 0;
+var curr_time = 0
+
+var aggression_state = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	target_entity = $"../Player"
-	target_pos = get_rand_pos(target_entity.position)
-
+	# start at a random position with a random target direction
+	position = get_rand_pos()
+	target_pos = get_rand_pos()
+	$Sus.hide()
+	$Alarm.hide()
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var velocity = (target_pos-position).normalized() * speed
+	var velocity = (target_pos).normalized() * speed
 		
 	# update player position based on velocity
 	if position.distance_to(target_pos) > 1:
@@ -26,13 +31,38 @@ func _process(delta: float) -> void:
 	# position timer - have the entity wander to a diff position every 5 seconds
 	curr_time += delta
 	if(curr_time >= wander_time):
-		target_pos = get_rand_pos(target_entity.position)
+		target_pos = get_rand_pos()
 		curr_time = 0;
+		
+	set_aggression_VFX()
 
 # function to get a random position within the range
-func get_rand_pos(base_pos: Vector2) -> Vector2:
+func get_rand_pos() -> Vector2:
 	var random_angle = randf_range(0.0, TAU) # TAU is 2 * PI
-	var random_radius = randf_range(0.0, search_dist)
-	print(base_pos + Vector2(cos(random_angle), sin(random_angle)) * random_radius)
-	return base_pos + Vector2(cos(random_angle), sin(random_angle)) * random_radius
+	var random_radius = randf_range(search_dist_min, search_dist_max)
+	return Vector2(cos(random_angle), sin(random_angle)) * random_radius
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:  # detect magic areas used by player
+	print(area.name)
+	if (area.is_in_group('Magical')):
+		print('MAGE DETECTED')
+		aggression_state += 1
+
+
+func _on_detection_field_area_exited(area: Area2D) -> void:
+	if (area.is_in_group('Magical')):
+		aggression_state -= 1
 	
+func set_aggression_VFX() -> void: #set the vfx vased on the aggression level
+	# sus is a question mark - cop is curious
+	# Alarm is an exclamation point - cop WILL attack
+	if (aggression_state >= 2): 
+		$Sus.hide()
+		$Alarm.show()
+	elif (aggression_state >= 1):
+		$Sus.show()
+		$Alarm.hide()
+	else:
+		$Sus.hide()
+		$Alarm.hide()
--- Player/player.gd
+++ Player/player.gd
@@ -1,7 +1,6 @@
-extends Area2D
+extends RigidBody2D
 
 # player stats
-
 const MAX_MANA = 100
 var mana = MAX_MANA / 2
 
@@ -10,15 +9,17 @@ var spell_state = false
 const MAX_SUS = 5
 var sus = 1
 
-const BASE_SPEED = 400
+const BASE_SPEED = 200
 var speed = BASE_SPEED
 
 var money = 0;
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	update_mana_ui()
 	update_sus_ui()
+	levitation_spell(false)
 	$mana.max_value = MAX_MANA
 	$sus.max_value = MAX_SUS
 	
@@ -58,11 +59,13 @@ func _process(delta: float) -> void:
 		if (spell_state == true):			# if spell is already in use, player can deactivate it free of charge
 			spell_state = false
 			speed = BASE_SPEED
+			levitation_spell(false)			# deactivate the spell
 			
-		elif (mana > 10):			# otherwise, if the player has some mana, cast the spell
+		elif (mana > 10):					# otherwise, if the player has some mana, cast the spell
 			spell_state = true
 			mana -= 10
 			speed = BASE_SPEED * 2
+			levitation_spell(true)			# activate the spell
 	
 	if (mana < MAX_MANA):
 		mana += delta
@@ -73,3 +76,16 @@ func _process(delta: float) -> void:
 	update_mana_ui()
 	update_sus_ui()
 	
+func levitation_spell(toggleOn: bool) -> void:
+	$LevitationField.visible = toggleOn
+	$LevitationField/LevitationShape.set_deferred("disabled", !toggleOn)
+
+
+func _on_levitation_field_body_entered(body: Node2D) -> void:
+	pass
+	# print('body')
+
+
+func _on_levitation_field_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+	pass
+	# print('shape') # Replace with function body.
